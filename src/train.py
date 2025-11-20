4d9af71a41a7a497a5c15f20e41f4292dea04e65
morenoblue <morenoblue2000@gmail.com>
2025-11-20 19:46:10 +0000

    wanna be

--- src/train.py
+++ src/train.py
@@ -7,7 +7,8 @@ from math import sqrt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
-# I'm adding this cuz I want the workflow to run again
+# I'm adding this cuz I want the workflow to run agai
+
 
 mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
 
